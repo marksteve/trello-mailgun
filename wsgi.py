38397a18583291ad09dd64740bb2fd8eeb4ca65c
Mark Steve Samson <blazinggale@gmail.com>
2012-09-05 13:24:38 +0800

    Added card creation

--- wsgi.py
+++ wsgi.py
@@ -1,20 +1,20 @@
 import settings
 
 import json
+import re
 import requests
 import urllib
 import urlparse
 
 from flask import (Flask, url_for, redirect, request, session, render_template,
-                   flash)
+                   flash, abort, Response)
 from flask_sqlalchemy import SQLAlchemy
 from oauth_hook import OAuthHook
 from sqlalchemy.dialects.mysql import VARCHAR
 
-TRELLO_REQUEST_TOKEN_URL = 'https://trello.com/1/OAuthGetRequestToken'
-TRELLO_AUTHORIZE_URL = 'https://trello.com/1/OAuthAuthorizeToken'
-TRELLO_ACCESS_TOKEN_URL = 'https://trello.com/1/OAuthGetAccessToken'
+TRELLO_API_ENDPOINT = 'https://trello.com/1/%s'
 TRELLO_SCOPES = 'read,write'
+EMAIL_PAT = re.compile('<?(\w+@[^@>]+)>?$')
 
 
 app = application = Flask(__name__)
@@ -24,6 +24,10 @@ app.config.update(
     SQLALCHEMY_DATABASE_URI=settings.SQLALCHEMY_DATABASE_URI,
 )
 db = SQLAlchemy(app)
+consumer = {
+    'consumer_key': settings.TRELLO_API_KEY,
+    'consumer_secret': settings.TRELLO_API_SECRET,
+}
 
 
 class User(db.Model):
@@ -36,15 +40,12 @@ class User(db.Model):
 @app.route('/setup', methods=['GET', 'POST'])
 def setup():
     oauth_verifier = request.args.get('oauth_verifier')
-    consumer = {
-        'consumer_key': settings.TRELLO_API_KEY,
-        'consumer_secret': settings.TRELLO_API_SECRET,
-    }
     if not oauth_verifier:
         # Get request token
         oauth_hook = OAuthHook(**consumer)
         trello_client = requests.session(hooks={'pre_request': oauth_hook})
-        response = trello_client.post(TRELLO_REQUEST_TOKEN_URL)
+        response = trello_client.post(TRELLO_API_ENDPOINT %
+                                      'OAuthGetRequestToken')
         request_token = dict(urlparse.parse_qsl(response.content))
         session['request_token'] = request_token
         # Authorize
@@ -55,7 +56,8 @@ def setup():
             'name': 'Trello-Mailgun',
             'expiration': 'never',
         }
-        return redirect(TRELLO_AUTHORIZE_URL + '?' + urllib.urlencode(params))
+        return redirect(TRELLO_API_ENDPOINT % 'OAuthGetRequestToken' + '?' +
+                        urllib.urlencode(params))
     else:
         # Get access token
         request_token = session.get('request_token')
@@ -64,8 +66,9 @@ def setup():
             access_token_secret=request_token['oauth_token_secret'],
             **consumer)
         trello_client = requests.session(hooks={'pre_request': oauth_hook})
-        response = trello_client.post(TRELLO_ACCESS_TOKEN_URL,
-                                      data={'oauth_verifier': oauth_verifier})
+        response = trello_client.post(
+            TRELLO_API_ENDPOINT % 'OAuthGetRequestToken',
+            data={'oauth_verifier': oauth_verifier})
         access_token = dict(urlparse.parse_qsl(response.content))
         # Get user details
         oauth_hook = OAuthHook(
@@ -92,6 +95,52 @@ def setup():
                 is_stored = True
         return render_template('setup.html', trello_user=trello_user,
                                is_stored=is_stored)
+
+
+def _get_email(s):
+    match = EMAIL_PAT.search(s)
+    if match:
+        return match.group(1)
+    else:
+        return
+
+
+@app.route('/create_card/<list_id>')
+def create_card(list_id):
+    # Get user
+    email = _get_email(request.form['From'])
+    if not email:
+        abort(400)
+    user = User.filter_by(email=email).first_or_404()
+    # Set trello client
+    oauth_hook = OAuthHook(
+        access_token=user.oauth_token,
+        access_token_secret=user.oauth_token_secret,
+        **consumer)
+    trello_client = requests.session(hooks={'pre_request': oauth_hook})
+    # Create card
+    data = {
+        'name': request.form['Subject'],
+        'desc': request.form['body-plain'],
+        'idList': list_id,
+    }
+    response = trello_client.post(TRELLO_API_ENDPOINT % 'cards', data=data)
+    card = json.loads(response.content)
+    # Add members from cc addresses
+    for cc in request.form['Cc'].split(','):
+        email = _get_email(cc)
+        if not email:
+            continue
+        cc_user = User.filter_by(email=email).first()
+        if not cc_user:
+            continue
+        data = {
+            'value': cc_user.id,
+        }
+        trello_client.post(
+            TRELLO_API_ENDPOINT % 'cards/%s/members' % card['id'],
+            data)
+    return Response()
 
 
 if __name__ == '__main__':
